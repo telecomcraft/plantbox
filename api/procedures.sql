3a760a639eef0a2d1ea2d9a20a3b146fd576b6e7
Eron Lloyd <eronlloyd@users.noreply.github.com>
2025-09-06 15:41:42 -0400

    Update procedures.sql
    Signed-off-by: Eron Lloyd <eronlloyd@users.noreply.github.com>

--- api/procedures.sql
+++ api/procedures.sql
@@ -2,7 +2,7 @@
 -- Reports the current version of the Plantbox geodatabase HTTP API
 CREATE OR REPLACE FUNCTION api.api_version() RETURNS INTEGER AS $$
 BEGIN
-    RETURN 1;
+    RETURN 0.1;
 END;
 $$ language plpgsql;
 ALTER FUNCTION api.api_version() OWNER TO authenticator;
